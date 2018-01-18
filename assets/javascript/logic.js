7bf7784066179227157fb2e8a7ed286529f13430
Nathan Jahnke <naterased@gmail.com>
2018-01-17 22:20:08 -0600

    added stars functionality and added box shadow and hover shadow effect on cards

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,9 +1,11 @@
 //Declare global variables
 
- var input = document.getElementById('location');
- var autocomplete = new google.maps.places.Autocomplete(input, {types: ['(cities)']});
- var globalCity = "";
- var globalState = "";
+var input = document.getElementById('location');
+var autocomplete = new google.maps.places.Autocomplete(input, {
+    types: ['(cities)']
+});
+var globalCity = "";
+var globalState = "";
 
 //Initialize Firebase
 var config = {
@@ -13,47 +15,47 @@ var config = {
     projectId: "foodie-travel-vault",
     storageBucket: "foodie-travel-vault.appspot.com",
     messagingSenderId: "458792787291"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
- var database = firebase.database();
+var database = firebase.database();
 
- $(document).ready(function() {
+$(document).ready(function () {
 
-//Google Maps API
-    google.maps.event.addListener(autocomplete, 'place_changed', function(){
-             var place = autocomplete.getPlace();
-              
-             var city = place. address_components[0].short_name;
-             
-             var state = place. address_components[2].short_name;
-                        
-              globalCity=city;
-              globalState=state;
-          
-        }); 
+    //Google Maps API
+    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        var place = autocomplete.getPlace();
 
-//Function to push Yelp API return object's parameters to Firebase
+        var city = place.address_components[0].short_name;
+
+        var state = place.address_components[2].short_name;
+
+        globalCity = city;
+        globalState = state;
+
+    });
+
+    //Function to push Yelp API return object's parameters to Firebase
     function saveToFireBase(name, addr1, addr2, phone, rating, photo, website) {
-       var newChildRef= database.ref().push({
-            dbRestaurantName:name,
-            dbAddress1:addr1,
-            dbAddress2:addr2,
-            dbPhoneNumber:phone,
-            dbRating:rating,
-            dbPhoto:photo,
-            dbWebsite:website
+        var newChildRef = database.ref().push({
+            dbRestaurantName: name,
+            dbAddress1: addr1,
+            dbAddress2: addr2,
+            dbPhoneNumber: phone,
+            dbRating: rating,
+            dbPhoto: photo,
+            dbWebsite: website
         });
-      }
+    }
 
-//Add an event listener to the database to pass parameters and evoke function to render Bootstrap cards when a new restaurant is added to thedatabase and on window load-load
-    database.ref().on("child_added",function(snapshot){
+    //Add an event listener to the database to pass parameters and evoke function to render Bootstrap cards when a new restaurant is added to thedatabase and on window load-load
+    database.ref().on("child_added", function (snapshot) {
         //Assign database values to variables
         var sv = snapshot.val();
         console.log(snapshot);
         //console log the unique Firebase ID
         console.log(snapshot.key);
-     
+
         renderCards(snapshot.key,
             sv.dbPhoto,
             sv.dbRestaurantName,
@@ -63,136 +65,137 @@ var config = {
             sv.dbRating
         )
 
-         $( function() {
-        $( ".datepicker" ).datepicker();
+        $(function () {
+            $(".datepicker").datepicker();
         });
     });
 
-  //Main function that calls Yelp API when a user enters a location and restaurant
-function retrieveAndDisplayRecordsViaYelpAPI() {
-     
-    var restaurantName = $("#restaurant-name").val().trim();
-    
-    //Empty out input values
-     $("#restaurant-name").val("");
-     $("#location").val("");
+    //Main function that calls Yelp API when a user enters a location and restaurant
+    function retrieveAndDisplayRecordsViaYelpAPI() {
 
-    var settings = {
-       "async": true,
-       "crossDomain": true,
-        "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location="+globalCity+","+globalState+"&term="+restaurantName+"&limit=1",
-       "method": "GET",
-       "headers": {
-        "authorization": "Bearer UVD6_jknwmi1GkRxFFkWh7HX-JV_TrlieWHvaSMfi69lmcN6MPUUxk5EGJNnUWmLD3TV5vtrZ25whGiC9gmJRUbhEW5lz3Y6hOXT5oAS-WqK7U5TA772Lt4tBqlaWnYx",
-           "Cache-Control": "no-cache",
-        }
-    };  //Initiating Ajax call
-       $.ajax(settings).done(function (response) {
-     
-      //JSON parameters based on the business search that returns an array
-        var responseObject = response.businesses[0];
-        var name = responseObject.name;
-        var address1 = responseObject.location.display_address[0];
-        var address2=responseObject.location.display_address[1];
-        var phoneNumber = responseObject.display_phone;
-        var rating = responseObject.rating;
-        var photo = responseObject.image_url;
-        var website = responseObject.url;
-        saveToFireBase(name,address1,address2,phoneNumber,rating,photo,website);
+        var restaurantName = $("#restaurant-name").val().trim();
 
-    });   //end of AJAX
+        //Empty out input values
+        $("#restaurant-name").val("");
+        $("#location").val("");
 
-}//end function retrieveAndDisplayRecordsViaYelpAPI()
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + globalCity + "," + globalState + "&term=" + restaurantName + "&limit=1",
+            "method": "GET",
+            "headers": {
+                "authorization": "Bearer UVD6_jknwmi1GkRxFFkWh7HX-JV_TrlieWHvaSMfi69lmcN6MPUUxk5EGJNnUWmLD3TV5vtrZ25whGiC9gmJRUbhEW5lz3Y6hOXT5oAS-WqK7U5TA772Lt4tBqlaWnYx",
+                "Cache-Control": "no-cache",
+            }
+        }; //Initiating Ajax call
+        $.ajax(settings).done(function (response) {
 
-//function to render cards using arguments passed by 
- function renderCards(id,photo,name,address1,address2,phoneNumber,rating){
- 
+            //JSON parameters based on the business search that returns an array
+            var responseObject = response.businesses[0];
+            var name = responseObject.name;
+            var address1 = responseObject.location.display_address[0];
+            var address2 = responseObject.location.display_address[1];
+            var phoneNumber = responseObject.display_phone;
+            var rating = responseObject.rating;
+            var photo = responseObject.image_url;
+            var website = responseObject.url;
+            saveToFireBase(name, address1, address2, phoneNumber, rating, photo, website);
+
+        }); //end of AJAX
+
+    } //end function retrieveAndDisplayRecordsViaYelpAPI()
+
+    //function to render cards using arguments passed by 
+    function renderCards(id, photo, name, address1, address2, phoneNumber, rating) {
+
         // var webLink = $("<a>").attr({
         //     "id":"url",
         //     "href":website,
         //     "target":"_blank"
         // });
-    //Restaurant image
-    var displayImage = $("<img>").attr({
-       "src": photo,
-        "id":"image",
-       "class":"card-img-top"
+        //Restaurant image
+        var displayImage = $("<img>").attr({
+            "src": photo,
+            "id": "image",
+            "class": "card-img-top"
+        });
+        //Restaurant name
+        var displayName = $("<h4>").attr({
+            "id": "name",
+            "class": "card-title"
+        });
+        displayName.text(name);
+
+        // Address display
+        var addressHeader = $("<h6>").text("Address");
+        var displayAddress = $("<p>").attr({
+            "id": "address",
+            "class": "card-text"
+        });
+        displayAddress.html(address1 + '<br />' + address2);
+
+        // Phone number display
+        var phoneHeader = $("<h6>").text("Phone Number");
+        var displayPhone = $("<p>").attr({
+            "id": "phone",
+            "class": "card-text"
+        });
+        displayPhone.text(phoneNumber);
+
+        //Rating
+
+        var ratingHeader = $("<h6>").text("Rating");
+        var displayRating = $("<p>").attr({
+            "class": "card-text rating"
+        });
+
+        displayRating.raty({
+            readOnly: true,
+            score: rating
+        })
+
+        //Adding button
+        var addDateButton = $("<button>").attr({
+            "type": "button",
+            "class": "add-date btn btn-primary btn-sm",
+            "id": id
+        });
+        addDateButton.text("Add Date");
+
+
+        var datePicker = $("<input>").attr({
+            "class": "datepicker form-control-sm",
+            "type": "text",
+        });
+
+        //Putting together the card       
+
+        var cardColumn = $("<div>").addClass("col-sm-3");
+
+        var card = $("<div>").addClass("card h-100");
+
+        var cardBlock = $("<div>").addClass("card-block");
+
+        $(".row").append(cardColumn);
+
+        card.append(displayImage).append(displayName).append(displayName).append(addressHeader).append(displayAddress).append(phoneHeader).append(displayPhone).append(ratingHeader).append(displayRating).append(datePicker).append(addDateButton);
+
+        card.prependTo(cardColumn);
+
+
+
+    } //end of render function
+
+    $(".container").on("click", ".add-date", function (event) {
+        var card = $(this).parent().children("input").val();
+        console.log(card)
     });
-    //Restaurant name
-    var displayName = $("<h4>").attr({
-        "id":"name",
-        "class":"card-title"
-    });
-    displayName.text(name);
 
-    // Address display
-    var addressHeader = $("<h6>").text("Address");
-    var displayAddress = $("<p>").attr({
-         "id":"address",
-         "class":"card-text"
-    });
-    displayAddress.html(address1+'<br />' + address2);
-
-    // Phone number display
-    var phoneHeader = $("<h6>").text("Phone Number");
-    var displayPhone = $("<p>").attr({
-        "id":"phone",
-        "class":"card-text"
-    });
-    displayPhone.text(phoneNumber);
-
-    //Rating
-
-    var ratingHeader = $("<h6>").text("Rating");
-    var displayRating = $("<p>").attr({
-        "id":"rating",
-        "class":"card-text"
-    });
-    displayRating.text(rating);
-
-    //Adding button
-     var addDateButton = $("<button>").attr({
-        "type":"button",
-         "class":"add-date btn btn-primary btn-sm",
-         "id":id
-     });
-     addDateButton.text("Add Date");
-
-
-     var datePicker = $("<input>").attr({
-        "class":"datepicker form-control-sm",
-        "type":"text",
+    //Submit button click event 
+    $("#search-button").on("click", function (event) {
+        event.preventDefault();
+        retrieveAndDisplayRecordsViaYelpAPI();
     });
 
-    //Putting together the card       
-       
-    var cardColumn = $("<div>").addClass("col-sm-3");
-
-    var card = $("<div>").addClass("card h-100");
-   
-    var cardBlock = $("<div>").addClass("card-block");
-
-    $(".row").append(cardColumn);
-
-    card.append(displayImage).append(displayName).append(displayName).append(addressHeader).append(displayAddress).append(phoneHeader).append(displayPhone).append(ratingHeader).append(displayRating).append(datePicker).append(addDateButton);
-
-    card.prependTo(cardColumn);
-
-   
-
-} //end of render function
-
-$(".container").on("click",".add-date", function(event){
-    var card = $(this).parent().children("input").val();
-    console.log(card)
 });
-         
- //Submit button click event 
-    $("#search-button").on("click", function(event) {
-       event.preventDefault();
-       retrieveAndDisplayRecordsViaYelpAPI();
-     });
-
-});
-
-
