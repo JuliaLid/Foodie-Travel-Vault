c01521172601bb68428b0bf406a2a5f4479b0a4b
Julia Lidstrom <juleslidstrom@gmail.com>
2018-01-23 19:43:03 -0600

    Final cleanup and commenting of files

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -7,7 +7,6 @@ var globalCity;
 var globalState;
 var itemInfo;
 
-
 //Initialize Firebase
 var config = {
     apiKey: "AIzaSyCoyQsX5G0nDGZMT1fuYTk-PTQ1WSevBFw",
@@ -21,12 +20,14 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
- $('#myModal').on('shown.bs.modal', function() {
-    $('#myInput').focus()
- })
-
-
+//Document ready functions
 $(document).ready(function () {
+
+    //Modal function
+     $('#myModal').on('shown.bs.modal', function() {
+        $('#myInput').focus()
+     });
+
     //Submit button click event 
     $("#search-button").on("click", function (event) {
         event.preventDefault();
@@ -44,7 +45,7 @@ $(document).ready(function () {
 
     });
 
-      //Google Maps API
+    //Google Maps API
     google.maps.event.addListener(autocomplete, 'place_changed', function () {
         var place = autocomplete.getPlace();
         var city = place.address_components[0].short_name;
@@ -59,7 +60,7 @@ $(document).ready(function () {
     function yelpAutocomplete() {
         var restaurantText = $("#restaurant-name").val();
 
-        // data request to yelp which uses the settings in buildSearchSettings function 
+        // data request to Yelp which uses the settings in buildSearchSettings function 
         var getData = function(request, response) {
             var settings = buildSearchSettings(request.term);
 
@@ -91,7 +92,6 @@ $(document).ready(function () {
                 itemInfo = null;
             }
         })
-
             // Formats the businesses we retrieve from the Yelp API query into a dropdown list. The number of items in dropdown depends on the limit we set in buildSearchSettings
             .autocomplete("instance")._renderItem = function(ul, item) {
                 return $("<li>")
@@ -102,59 +102,20 @@ $(document).ready(function () {
     };
     yelpAutocomplete();
 
-    //jPList readiness checker
-    $('#demo').jplist({
-        itemsBox: '.list',
-        itemPath: '.list-item',
-        panelPath: '.jplist-panel'
-    });
-
-    //Function to push Yelp API return object's parameters to Firebase
-    function saveToFireBase(name, addr1, addr2, phone, rating, photo, website) {
-        var newChildRef = database.ref().push({
-            dbRestaurantName: name,
-            dbAddress1: addr1,
-            dbAddress2: addr2,
-            dbPhoneNumber: phone,
-            dbRating: rating,
-            dbPhoto: photo,
-            dbWebsite: website,
-       });
+     //Function to build the API call based on the user selections
+    function buildSearchSettings(restaurantName) {
+        return {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + globalCity + "," + globalState + "&term=" + restaurantName + "&limit=8",
+            "method": "GET",
+            "headers": {
+                "authorization": "Bearer UVD6_jknwmi1GkRxFFkWh7HX-JV_TrlieWHvaSMfi69lmcN6MPUUxk5EGJNnUWmLD3TV5vtrZ25whGiC9gmJRUbhEW5lz3Y6hOXT5oAS-WqK7U5TA772Lt4tBqlaWnYx",
+                "Cache-Control": "no-cache",
+            }
+        };
     }
-
-    //Add an event listener to the database to pass parameters and evoke function to render Bootstrap cards when a new restaurant is added to thedatabase and on window load-load
-    database.ref().on("child_added", function (snapshot) {
-        //Assign database values to variables
-        var sv = snapshot.val();
-        console.log(snapshot);
-        //console log the unique Firebase ID
-        console.log(snapshot.key);
-
-        //if sv has dbDate, then take date and display it
-        //if the date does not exist (which it may not initially) then display an empty string.
-        console.log(" Does date field exist? '" + sv.hasOwnProperty('dbDate').toString() + "'");
-        var dateVar = "";
-        if (sv.hasOwnProperty('dbDate')) {
-            dateVar = sv.dbDate;
-        }
-
-        renderCards(snapshot.key,
-            sv.dbPhoto,
-            sv.dbRestaurantName,
-            sv.dbAddress1,
-            sv.dbAddress2,
-            sv.dbPhoneNumber,
-            sv.dbRating,
-            sv.dbWebsite,
-			dateVar
-        )
-
-        $(function () {
-            $(".datepicker").datepicker();
-        });
-    });
-
-    //Main function that calls Yelp API when a user enters a location and restaurant
+    //Ajax function that calls Yelp API when a user enters a location and restaurant
     function retrieveAndDisplayRecordsViaYelpAPI() {
         //Grab user input
         var restaurantName = $("#restaurant-name").val().trim();
@@ -171,6 +132,7 @@ $(document).ready(function () {
         }); //end of AJAX
     } //end function retrieveAndDisplayRecordsViaYelpAPI()
 
+    //Function to capture specific parameters from a returned JSON object
     function firebaseDataPrep(responseObject) {
         var name = responseObject.name;
         var address1 = responseObject.location.display_address[0];
@@ -182,24 +144,52 @@ $(document).ready(function () {
         saveToFireBase(name, address1, address2, phoneNumber, rating, photo, website);
     }
 
-    function buildSearchSettings(restaurantName) {
-        return {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + globalCity + "," + globalState + "&term=" + restaurantName + "&limit=8",
-            "method": "GET",
-            "headers": {
-                "authorization": "Bearer UVD6_jknwmi1GkRxFFkWh7HX-JV_TrlieWHvaSMfi69lmcN6MPUUxk5EGJNnUWmLD3TV5vtrZ25whGiC9gmJRUbhEW5lz3Y6hOXT5oAS-WqK7U5TA772Lt4tBqlaWnYx",
-                "Cache-Control": "no-cache",
-            }
-        };
+    //Function to push Yelp API return object's parameters to Firebase
+    function saveToFireBase(name, addr1, addr2, phone, rating, photo, website) {
+        var newChildRef = database.ref().push({
+            dbRestaurantName: name,
+            dbAddress1: addr1,
+            dbAddress2: addr2,
+            dbPhoneNumber: phone,
+            dbRating: rating,
+            dbPhoto: photo,
+            dbWebsite: website,
+       });
     }
 
-    //function to render cards using arguments passed in 
+    //Add an event listener to the database to pass parameters and evoke function to render Bootstrap cards when a new restaurant is added to the database and on window load
+    database.ref().on("child_added", function (snapshot) {
+        //Assign database values to variables
+        var sv = snapshot.val();
+        //console log the unique Firebase ID
+        console.log(snapshot.key);
+        //if sv has dbDate, then take date and display it
+        //if the date does not exist (which it may not initially) then display an empty string.
+        var dateVar = "";
+        if (sv.hasOwnProperty('dbDate')) {
+            dateVar = sv.dbDate;
+        }
+        //Pass database values as arguments to a function that renders a card for each restaurant
+        renderCards(snapshot.key,
+            sv.dbPhoto,
+            sv.dbRestaurantName,
+            sv.dbAddress1,
+            sv.dbAddress2,
+            sv.dbPhoneNumber,
+            sv.dbRating,
+            sv.dbWebsite,
+			dateVar
+        )
+        
+        //jQuery date picker 
+        $(function () {
+            $(".datepicker").datepicker();
+        });
+    });
 
+   //Function to render cards using arguments passed in from database
     function renderCards(id, photo, name, address1, address2, phoneNumber, rating, website, date) {
         //Adding a link to the Yelp search result for the restaurant
-        console.log(website);
         var webLink = $("<a>").attr({
             "id":"url",
             "href":website,
@@ -282,31 +272,27 @@ $(document).ready(function () {
         $(".row").prepend(cardColumn);
         card.append(webLink).append(displayName).append(addressHeader).append(displayAddress).append(phoneHeader).append(displayPhone).append(ratingHeader).append(displayRating).append(selectDateHeader).append(datePicker).append(addDateButton).append(deleteRestaurant);
 
-
         card.prependTo(cardColumn);
     } //end of renderCards function
 
+    //Add a selected date to the database
     $(".container").on("click", ".add-date", function (event) {
         //get the date from the date text field
-        var indate = $(this).parent().children("input").val().trim();
+        var inDate = $(this).parent().children("input").val().trim();
         //get firebase id from the 'add-date' button that was clicked
         var firebaseId = $(this).attr("id");
-        console.log("In add-date firebaseId is=" + firebaseId);
-
         //add the date info to the database
         database.ref().child(firebaseId).update(
-            { dbDate: indate }
+            { dbDate: inDate }
         );
     });
 
-    //remove a restaurant
+    //Remove a restaurant from the database and page when the trash icon is clicked
     $(".container").on("click", ".fa-trash-o", function (event) {
         event.preventDefault();
         var firebaseId = $(this).attr("fid");
-        console.log("trash button clicked with firebase id=" + firebaseId);
-        console.log("removing object with firebase id=" + firebaseId);
         var deleteResult = database.ref().child(firebaseId).remove(function (error) {
-            console.log("During remove of " + firebaseId + " an error occurred " + error);
+            console.log("During removal of " + firebaseId + " an error occurred " + error);
         });
         //refresh browser to reload database
         window.location.reload();
